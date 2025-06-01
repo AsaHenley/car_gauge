d0180ae0ab278f9440780a2ae48c929c5bcdcab0
Asa Henley <spheal25@gmail.com>
2025-06-01 11:07:13 -0500

    Added obd connections

--- car_gauge_api/obd_api.py
+++ car_gauge_api/obd_api.py
@@ -1,17 +1,42 @@
 from fastapi import FastAPI
+import obd
 import random
 
 app = FastAPI()
+connection = obd.OBD()
 
 @app.get("/")
 def read_root():
     return getObdData()
 
 
-
 def getObdData():
     # Read obd data here, parse it and return it
-    speed = random.randint(10, 140)
-    fuel = random.randint(10, 90)
-    return {"speed": speed,
-            "fuel": fuel}
+    if(connection.status == obd.OBDStatus.NOT_CONNECTED):
+        print('No Obd Connection')
+        speed = random.randint(10, 140)
+        rpm = random.randint(1000, 3500)
+        coolant_temp = random.randint(80, 110) # C
+        throttle = random.randint(0, 100) # %
+        run_time = obd.commands.RUN_TIME # Sec
+        fuel = random.randint(0, 100)# %
+        oil_temp = random.randint(80, 110) # C
+        
+        fuel = random.randint(10, 90)
+    else:
+        speed = obd.commands.SPEED # kph
+        rpm = obd.commands.RPM # rmp
+        coolant_temp = obd.commands.COOLANT_TEMP # C
+        throttle = obd.commands.THROTTLE_POS # %
+        run_time = obd.commands.RUN_TIME # Sec
+        fuel = obd.commands.FUEL_LEVEL# %
+        oil_temp = obd.commands.OIL_TEMP # C
+    return {
+            "speed": speed,
+            "rpm": rpm,
+            "coolant_temp": coolant_temp,
+            "throttle": throttle,
+            "run_time": run_time,
+            "fuel": fuel,
+            "oil_temp": oil_temp
+            }
---
+++ start_gauges.bat
@@ -0,0 +1,3 @@
+cd .venv\Scripts
+fastapi dev ..\..\car_gauge_api\obd_api.py
+cmd
